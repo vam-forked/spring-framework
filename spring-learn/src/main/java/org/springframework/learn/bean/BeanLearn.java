229b795c1fac26c23db9e2fbde919c3acd2e8e0d
fukuankuan <fukuankuan@youzan.com>
2020-03-11 17:19:51 +0800

    [add] 新增spring-learn模块，用于spring源码的阅读

---
+++ spring-learn/src/main/java/org/springframework/learn/bean/BeanLearn.java
@@ -0,0 +1,17 @@
+package org.springframework.learn.bean;
+
+/**
+ * @author vam
+ * @version 1.0
+ * @date 2019/12/12 8:30 下午
+ * @see
+ */
+public class BeanLearn {
+
+	public static void main(String[] args) {
+
+		ClassPathXml
+
+	}
+
+}
---
+++ spring-learn/src/main/java/org/springframework/learn/bean/base/Student.java
@@ -0,0 +1,22 @@
+package org.springframework.learn.bean.base;
+
+/**
+ * @author vam
+ * @version 1.0
+ * @date 2019/12/12 8:32 下午
+ * @see
+ */
+public class Student {
+
+	private Integer age;
+
+	private String name;
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
